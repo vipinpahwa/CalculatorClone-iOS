5c316e44a49f5be684de030752d489b7469282cd
vipin.pahwa <vipin.pahwa@go-jek.com>
2021-06-28 23:42:22 +0530

    Add numberLabel

--- CalculatorClone/ViewController.swift
+++ CalculatorClone/ViewController.swift
@@ -8,13 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var numberLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.numberOfLines = 1
+        label.text = "0"
+        label.textColor = .white
+        label.textAlignment = .right
+        label.font = UIFont(name: "Helvetica", size: 50)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
         self.view.backgroundColor = UIColor.black
     }
 
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSubviews()
+    }
+    
+    private func setupSubviews() {
+        addNumberLabel()
+    }
+    
+    private func addNumberLabel() {
+        self.view.addSubview(numberLabel)
+        
+        NSLayoutConstraint.activate([
+            numberLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            numberLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+            numberLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100)
+        ])
+    }
 }
-
